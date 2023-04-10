07eccbcb7dff3884ae34f5941af0c0f774eeb18a
Mahesh Kumar Jaiswal <87239664+MKJ07@users.noreply.github.com>
2023-04-10 23:39:38 +0530

    SQL 1 Day 2

--- LeetCode 1873.sql
+++ LeetCode 1873.sql
@@ -1,3 +1,8 @@
 -- 1873. Calculate Special Bonus
+
+-- Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number 
+-- and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+-- Return the result table ordered by employee_id.
+
 select employee_id,case when mod(employee_id,2)<>0 and name not like 'M%' then salary 
 else 0 end as bonus from Employees order by employee_id;
