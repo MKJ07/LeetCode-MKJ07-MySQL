3a0a4f975beb0475f5efcdc03234b6da9a64114e
Mahesh Kumar Jaiswal <87239664+MKJ07@users.noreply.github.com>
2023-04-10 23:42:19 +0530

    SQL 1 Day 1

--- LeetCode 584.sql
+++ LeetCode 584.sql
@@ -1,3 +1,6 @@
 -- 584. Find Customer Referee
+
+-- Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.Return the result table in any order.
+
 select name from Customer
 where referee_id is null or referee_id != 2;
