1c0191f3cd0bd819fb65d4cf8a18937acce07257
Mahesh Kumar Jaiswal <87239664+MKJ07@users.noreply.github.com>
2023-04-10 23:41:24 +0530

    SQL 1 Day 1

--- LeetCode 183.sql
+++ LeetCode 183.sql
@@ -1,3 +1,6 @@
 -- 183. Customers Who Never Order
+
+-- Write an SQL query to report all customers who never order anything.Return the result table in any order.
+
 select name as Customers from Customers c left join orders o on c.id=o.customerId
 where o.id is null; 
