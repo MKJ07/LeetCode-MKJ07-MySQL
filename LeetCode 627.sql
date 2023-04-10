ee8924e6a648d2f7c87626b0e772741bd6e757de
Mahesh Kumar Jaiswal <87239664+MKJ07@users.noreply.github.com>
2023-04-10 23:44:55 +0530

    SQL 1 Day 2

--- LeetCode 627.sql
+++ LeetCode 627.sql
@@ -1,3 +1,7 @@
 -- 627. Swap Salary.
-# Write your MySQL query statement below
+
+-- Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single 
+-- update statement and no intermediate temporary tables.Note that you must write a single update statement,
+-- do not write any select statement for this problem.
+
 update Salary set sex= case sex when 'm' then 'f' else 'm' end;
