ab5dbe698e8c9b3fd4e9bb4cf7560af0224ec497
Mahesh Kumar Jaiswal <87239664+MKJ07@users.noreply.github.com>
2023-05-30 22:22:56 +0530

    Database Pratice Problem

---
+++ LeetCode 610.sql
@@ -0,0 +1,7 @@
+-- 610. Triangle Judgement
+
+-- Write an SQL query to report for every three line segments whether they can form a triangle.
+-- Return the result table in any order.
+
+select x,y,z,case when (x+y>z and y+z>x and x+z>y) then 'Yes'
+else 'No' end as 'triangle' from Triangle;
